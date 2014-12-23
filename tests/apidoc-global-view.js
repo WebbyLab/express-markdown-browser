3d19df9229c3a850ff3f99d573f8386b818f22af
siavolt <siavolt@meta.ua>
2014-12-23 15:20:51 +0200

    Update tests

--- tests/apidoc-global-view.js
+++ tests/apidoc-global-view.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var port = 9950;
+var port = 8887;
 
 var assert  = require('chai').assert;
 var express = require('express');
@@ -20,23 +20,22 @@ app.get('/echo', function(req, res){
 app.use(emb);
 
 test('Positive: check route.', function(done) {
-    request.get("http://127.0.0.1:9950/echo", function optionalCallback(err, httpResponse, body) {
+    request.get("http://127.0.0.1:"+port+"/echo", function optionalCallback(err, httpResponse, body) {
         assert.deepEqual(body, "echo");
         done();
     });
 });
 
 test('Positive: Specification list.', function(done) {
-    request.get("http://127.0.0.1:9950/echosfdfvc", function optionalCallback(err, httpResponse, body) {
+    request.get("http://127.0.0.1:"+port+"/echosfdfvc", function optionalCallback(err, httpResponse, body) {
+
         var moviesRegExp = new RegExp("<h3>movies</h3>");
         var usersRegExp  = new RegExp("<h3>users</h3>");
-        var linkRegExp   = new RegExp("http://127.0.0.1:9950/movies/create.md");
-        var linkRegExp2  = new RegExp("http://127.0.0.1:9950/users/create.md");
 
         assert.ok(moviesRegExp.test(body));
         assert.ok(usersRegExp.test(body));
-        assert.ok(linkRegExp.test(body));
-        assert.ok(linkRegExp2.test(body));
+        assert.ok((/http:\/\/127\.0\.0\.1:8887\/echosfdfvc\?page=\/movies\/create/).test(body));
+        assert.ok((/http:\/\/127\.0\.0\.1:8887\/echosfdfvc\?page=\/users\/create/).test(body));
 
         link = body.match(/href=[\'"]?([^\'" >]+)/)[1];
 
